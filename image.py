821bd039715380a4aa2ac81d72c409d96b9fcf7a
Jacqueline Buford <jacqubu@umich.edu>
2023-10-03 17:35:18 -0400

    Initial commit

---
+++ image.py
@@ -0,0 +1,19 @@
+# use imageio to convert png into numpy resolution of image is 250*250 np array will be 3*250*250
+
+
+#resize image (function) - resize all image to 1 resolution
+
+
+
+#data cleaning
+
+
+
+
+#have all the image into a dataset in Pytorch
+
+
+
+#all label goes into dataset
+
+
---
+++ model.py
@@ -0,0 +1,13 @@
+# define 3 different convolution layers
+
+# define a maxpool layer
+
+
+#define the activation function
+
+
+#define neural network
+
+
+
+#define feed forward
